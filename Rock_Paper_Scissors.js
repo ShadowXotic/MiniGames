90a3015a27fc674f510bb13b69dac697a6d1721b
ShadowXotic <vivek4shadowxaos@gmail.com>
2025-06-07 15:51:12 -0700

    Create Rock_Paper_Scissors.js
    Added 1 of 2 entries of Rock Paper Scissors in Javascript

---
+++ Rock_Paper_Scissors.js
@@ -0,0 +1,19 @@
+function play(userChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = '';
+
+    if (userChoice === computerChoice) {
+        result = `It's a tie! You both chose ${userChoice}.`;
+    } else if (
+        (userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'paper' && computerChoice === 'rock') ||
+        (userChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        result = `You win! ${userChoice} beats ${computerChoice}.`;
+    } else {
+        result = `You lose! ${computerChoice} beats ${userChoice}.`;
+    }
+
+    document.getElementById('result').innerText = result;
+}
